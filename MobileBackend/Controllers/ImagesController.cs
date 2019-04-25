2ce7811215cb4b7e89ff443ecddadd2c749647e2
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-04-25 18:18:03 +0200

    Created a simple method (inside controller) to upload images TODO: create method in service and repository

--- MobileBackend/Controllers/ImagesController.cs
+++ MobileBackend/Controllers/ImagesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MobileBackend.Commands;
 using MobileBackend.Services;
@@ -6,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MobileBackend.Controllers
@@ -31,6 +34,33 @@ namespace MobileBackend.Controllers
             var path = env.WebRootFileProvider.GetFileInfo($"images/{filename}.jpeg")?.PhysicalPath;
             var b = await System.IO.File.ReadAllBytesAsync(path);
             return File(b, "image/jpeg");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var uploads = Path.Combine(env.WebRootPath, "images");
+            var imageGuid = Guid.NewGuid().ToString() + ".jpeg";
+            var fullPath = Path.Combine(uploads, imageGuid);
+            if(file == null)
+            {
+                return Content("null");
+            }
+            if(file.Length > 0)
+            {
+                var builder = new StringBuilder();
+                builder.AppendLine($"name: {file.Name}");
+                builder.AppendLine($"filename: {file.FileName}");
+                builder.AppendLine($"content type: {file.ContentType}");
+                builder.AppendLine($"length: {file.Length}");
+                await file.CopyToAsync(new FileStream(fullPath, FileMode.Create));
+
+                return Content(builder.ToString());
+            }
+            else
+            {
+                return Content("file.Length > 0 is false");
+            }
         }
     }
 }   
